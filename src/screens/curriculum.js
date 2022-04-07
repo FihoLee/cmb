d1737ad816640f80a49995c2ac6bd363492bdd99
Sam <samlucas383@gmail.com>
2022-04-07 11:49:05 -0500

    fixed things

--- src/screens/curriculum.js
+++ src/screens/curriculum.js
@@ -1,52 +1,3 @@
-<<<<<<< Updated upstream
-import React, { Component } from "react";
-import { withAuthenticator } from "@aws-amplify/ui-react";
-import { Storage } from 'aws-amplify';
-
-
-
-class Curriculum extends Component {
-  state = { fileUrl: '', file: '', filename:'' }
-  handleChange = e => {
-    const file = e.target.files[0]
-    this.setState({
-      fileUrl: URL.createObjectURL(file),
-      file,
-      filename: file.name
-    })
-  }
-  saveFile = () => {
-    Storage.put(this.state.filename, this.state.file)
-    .then(() => {
-      console.log('successfully saved file!')
-      this.setState({ fileUrl: '', file: '', filename: '' })
-    })
-    .catch(err => {
-      console.log('error uploading file', err)
-    })
-  }
-  render () {
-    return(
-      <div className="Curr">
-        <header className="Curr-header">
-          <h1>
-            Curriculum
-          </h1>
-        </header>
-        <input type='file' onChange={this.handleChange} />
-        <img src={this.state.fileUrl} alt="your file" />
-        <p></p>
-        <button onClick={this.saveFile}>Save File</button>
-      </div>
-    );
-  }
-}
-//export default withAuthenticator(Curriculum, {includeGreetings: true})
-=======
-//import React, { useRef, useState } from 'react';
-//import 'bootstrap/dist/css/bootstrap.min.css';
-//import { storage } from './firebase';
-
 
 import React,{useState} from 'react'
 // Import the main component
@@ -140,5 +91,5 @@ export const Curriculum = () => {
 }
 
  
->>>>>>> Stashed changes
+
 export default Curriculum;
