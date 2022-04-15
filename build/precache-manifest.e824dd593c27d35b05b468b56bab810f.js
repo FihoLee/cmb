a492b2590ff98a377134fcda702e819cbb65dc94
FihoLee <fiholee@my.unt.edu>
2022-04-15 11:06:02 -0500

    merge test

---
+++ build/precache-manifest.e824dd593c27d35b05b468b56bab810f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a450a7d731e2f18fcc0148ad175f1f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9f5569b693efe7960f6",
+    "url": "/static/css/main.084ea700.chunk.css"
+  },
+  {
+    "revision": "c8a4e8f605bbda123243",
+    "url": "/static/js/2.38216116.chunk.js"
+  },
+  {
+    "revision": "767c9ca9f2c952c21badd7b8e2f37bd0",
+    "url": "/static/js/2.38216116.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9f5569b693efe7960f6",
+    "url": "/static/js/main.fd65d1e2.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "d8ee7763fdda277ac19a00af856980a4",
+    "url": "/static/media/ReadySetTeach.731305e8.jpg"
+  }
+]);
